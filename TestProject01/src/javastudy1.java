4e021658d06f918b5ef52fff57475556e3adc3b9
arxxxxxx <arimaore@gmail.com>
2018-01-19 21:40:48 +0900

    1/19

--- TestProject01/src/javastudy1.java
+++ TestProject01/src/javastudy1.java
@@ -3,10 +3,6 @@ public class javastudy1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int i = 0;
-		do{
-			System.out.println(i);
-			i++;
-		}while(i < 10);
+		
 	}
 }
---
+++ studyDB/src/studyDB/ChkLoadJdbc.java
@@ -0,0 +1,10 @@
+package studyDB;
+
+public class ChkLoadJdbc {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
